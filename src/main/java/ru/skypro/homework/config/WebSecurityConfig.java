9b8406f794ac1c5ab57b0859f3f805618dadf560
Mishator <voyteh89@mail.ru>
2024-08-20 16:56:56 +0300

    В классе WebSecurityConfig исправил реализацию filterChain.

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -1,20 +1,25 @@
 package ru.skypro.homework.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final MyUserDetailsService userDetailsService;
 
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
@@ -29,25 +34,21 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf()
-                .disable()
+                .disable().
+                userDetailsService(userDetailsService)
                 .authorizeHttpRequests(
                         authorization ->
                                 authorization
+                                        .antMatchers(HttpMethod.OPTIONS)
+                                        .permitAll()
                                         .mvcMatchers(AUTH_WHITELIST)
                                         .permitAll()
-                                        .mvcMatchers(HttpMethod.GET, "/listings")
+                                        .antMatchers(HttpMethod.GET, "/ads", "/ads/image/**", "/user/image/**")
                                         .permitAll()
-                                        .mvcMatchers("/listings/**", "/users/**")
+                                        .mvcMatchers("/ads/**", "/users/**")
                                         .authenticated())
-                .cors(cors -> cors
-                        .configurationSource(request -> {
-                            CorsConfiguration config = new CorsConfiguration();
-                            config.setAllowCredentials(true);
-                            config.addAllowedOrigin("http://localhost:3000");
-                            config.addAllowedMethod("*");
-                            config.addAllowedHeader("*");
-                            return config;
-                        }))
+                .cors()
+                .and()
                 .httpBasic(withDefaults());
         return http.build();
     }
